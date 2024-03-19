c50c256effa105a5c33ac2f01fe1876fb1f3fe22
Eva Corp <142232787+TehnikumCodingFrontend@users.noreply.github.com>
2024-03-19 07:14:52 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+  var splide = new Splide( '.splide' );
+  splide.mount();
+
+} );
+
+document.addEventListener( 'DOMContentLoaded', function() {
+  var splide = new Splide( '.splide2' );
+  splide.mount();
+  
+} );
