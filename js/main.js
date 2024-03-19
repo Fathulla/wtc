478370a7002ce777d0006ba9437f586ae232ddaf
Eva Corp <142232787+TehnikumCodingFrontend@users.noreply.github.com>
2024-03-19 17:03:32 +0500

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-  var splide = new Splide( '.splide' );
+  var splide = new Splide( '.splide', {
+    type: "slide",
+    perPage: 5,
+    drag: "free",
+  } );
   splide.mount();
 
 } );
 
 document.addEventListener( 'DOMContentLoaded', function() {
-  var splide = new Splide( '.splide2' );
+  var splide = new Splide( '.splide1', {
+    type: "slide",
+    perPage: 5,
+    drag: "free",
+  } );
   splide.mount();
   
 } );
+
+const myModal = new HystModal({
+  linkAttributeName: "data-hystmodal",
+  // настройки (не обязательно), см. API
+});
