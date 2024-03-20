833cf8ee5069c1fa3b32b7fc2f59c9beaf588f68
Eva Corp <142232787+TehnikumCodingFrontend@users.noreply.github.com>
2024-03-21 01:52:20 +0500

    minor changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
     type: "slide",
     perPage: 5,
     drag: "free",
+    gap: 30
   } );
   splide.mount();
 
@@ -13,6 +14,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
     type: "slide",
     perPage: 5,
     drag: "free",
+    gap: 30,
   } );
   splide.mount();
   
